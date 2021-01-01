45e19efe439a929d02fe5ca2d9ab7b595c8881b8
njayman <najishm828282@gmail.com>
2021-01-01 11:10:38 +0600

    fixed image cache problem

--- src/components/SlideMaterial.js
+++ src/components/SlideMaterial.js
@@ -1,29 +1,10 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import axios from 'axios'
-const { REACT_APP_NOT_AXIOS_BASE_URL } = process.env
+import React from 'react'
 
 export default function Material({ material }) {
-    const [materialObject, setMaterialObject] = useState({})
-    const getMaterialObject = useCallback(async () => {
-        try {
-            const { data } = await axios.get(`${REACT_APP_NOT_AXIOS_BASE_URL}/admin/getmaterial/${material}`)
-            if (data.success) {
-                console.log(data)
-                setMaterialObject(data.material)
-            } else {
-                console.log(data.message)
-            }
-        } catch (error) {
-            console.log(error.message)
-        }
-    }, [material])
-    useEffect(() => {
-        getMaterialObject()
-    }, [getMaterialObject])
     return (
         <>
-            {materialObject?.materialtype === 'Image' && <img src={materialObject?.material} style={{ width: "100vw" }} alt={materialObject.name} />}
-            {materialObject?.materialtype === 'Text' && <div className="text_slide"><h1>{materialObject?.material}</h1></div>}
+            {material?.materialtype === 'Image' && <img src={material?.material} style={{ width: "100vw" }} alt={material.name} />}
+            {material?.materialtype === 'Text' && <div className="text_slide"><h1>{material?.material}</h1></div>}
         </>
     )
 }
