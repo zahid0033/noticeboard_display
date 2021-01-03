65355df1a0c8d98fd668216b3b26b0125eab8467
njayman <najishm828282@gmail.com>
2021-01-03 12:43:02 +0600

    fix for single image

--- src/components/single_image/home.js
+++ src/components/single_image/home.js
@@ -3,9 +3,9 @@ import React from 'react'
 const SingleImage = ({ notice }) => {
     return (
         <div style={{ height: "100vh" }}>
-            <div>
+            <div style={{ height: "100%", position: "relative" }}>
                 <img src={notice.material.material} alt=""
-                    style={{ height: "100%" }} />
+                    style={{ height: "100%", display: "block", marginLeft: "auto", marginRight: "auto" }} />
                 <div style={{ position: "absolute", top: "10px", right: "10px" }}>
                     <img src="https://i.ibb.co/DKqStGC/download.png" alt="" style={{ width: "100px", borderRadius: "50%" }} />
                 </div>
