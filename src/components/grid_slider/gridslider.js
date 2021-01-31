01b10bf511de577b913e4bdbb225f31a97b60f96
njayman <najishm828282@gmail.com>
2021-01-31 08:44:43 +0600

    video display added

--- src/components/grid_slider/gridslider.js
+++ src/components/grid_slider/gridslider.js
@@ -1,51 +1,119 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from "react";
+import ReactPlayer from "react-player";
+
 function GridSlider({ notice }) {
-    const [idx, setIdx] = useState(0)
-    const [time, setTime] = useState(notice?.interval)
+  const [idx, setIdx] = useState(0);
+  const [time, setTime] = useState(notice?.interval);
 
-    useEffect(() => {
-        let changeSlide = setTimeout(() => {
-            if ((idx === notice.materials.length - 3) || (idx === notice.materials.length - 2) || (idx === notice.materials.length - 1)) {
-                setIdx(0)
-            } else {
-                setIdx(d => d + 3)
-            }
-        }, Number(notice.interval) * 1000)
-        return () => clearTimeout(changeSlide)
-    }, [notice.materials.length, idx, notice.interval])
+  useEffect(() => {
+    let changeSlide = setTimeout(() => {
+      if (
+        idx === notice.materials.length - 3 ||
+        idx === notice.materials.length - 2 ||
+        idx === notice.materials.length - 1
+      ) {
+        setIdx(0);
+      } else {
+        setIdx((d) => d + 3);
+      }
+    }, Number(notice.interval) * 1000);
+    return () => clearTimeout(changeSlide);
+  }, [notice.materials.length, idx, notice.interval]);
 
-    useEffect(() => {
-        let countDown = setInterval(() => {
-            if (time === 1) {
-                setTime(notice?.interval)
-            } else {
-                setTime(t => t - 1)
-            }
-        }, 1000)
-        return () => clearInterval(countDown)
-    }, [time, notice?.interval])
+  useEffect(() => {
+    let countDown = setInterval(() => {
+      if (time === 1) {
+        setTime(notice?.interval);
+      } else {
+        setTime((t) => t - 1);
+      }
+    }, 1000);
+    return () => clearInterval(countDown);
+  }, [time, notice?.interval]);
 
-    return (
-        <>
-            <div className="counter">
-                <h1>{time}</h1>
-                <p>Playing {idx + 1} of {notice?.materials?.length}</p>
+  return (
+    <>
+      <div className="counter">
+        <h1>{time}</h1>
+        <p>
+          Playing {idx + 1} of {notice?.materials?.length}
+        </p>
+      </div>
+      <div className="flex-container">
+        <Fragment>
+          {notice?.materials[idx]?.materialtype === "Video" && (
+            <div className="item">
+              <ReactPlayer
+                url={notice?.materials[idx].material}
+                playing={true}
+                loop={true}
+              />
             </div>
-            <div className="flex-container">
-                <Fragment>
-                    {notice?.materials[idx]?.materialtype === 'Image' && <div className="item"><img src={notice?.materials[idx].material} alt={notice?.materials[idx].name} /></div>}
-                    {notice?.materials[idx]?.materialtype === 'Text' && <div className="item"><h1>{notice?.materials[idx].material}</h1></div>}
-                </Fragment>
-                <Fragment>
-                    {notice?.materials[idx + 1]?.materialtype === 'Image' && <div className="item"><img src={notice?.materials[idx + 1].material} alt={notice?.materials[idx + 1].name} /></div>}
-                    {notice?.materials[idx + 1]?.materialtype === 'Text' && <div className="item"><h1>{notice?.materials[idx + 1].material}</h1></div>}
-                </Fragment>
-                <Fragment>
-                    {notice?.materials[idx + 2]?.materialtype === 'Image' && <div className="item"><img src={notice?.materials[idx + 2].material} alt={notice?.materials[idx + 2].name} /></div>}
-                    {notice?.materials[idx + 2]?.materialtype === 'Text' && <div className="item"><h1>{notice?.materials[idx + 2].material}</h1></div>}
-                </Fragment>
+          )}
+          {notice?.materials[idx]?.materialtype === "Image" && (
+            <div className="item">
+              <img
+                src={notice?.materials[idx].material}
+                alt={notice?.materials[idx].name}
+              />
             </div>
-        </>
-    )
+          )}
+          {notice?.materials[idx]?.materialtype === "Text" && (
+            <div className="item">
+              <h1>{notice?.materials[idx].material}</h1>
+            </div>
+          )}
+        </Fragment>
+        <Fragment>
+          {notice?.materials[idx + 1]?.materialtype === "Video" && (
+            <div className="item">
+              <ReactPlayer
+                url={notice?.materials[idx + 1].material}
+                playing={true}
+                loop={true}
+              />
+            </div>
+          )}
+          {notice?.materials[idx + 1]?.materialtype === "Image" && (
+            <div className="item">
+              <img
+                src={notice?.materials[idx + 1].material}
+                alt={notice?.materials[idx + 1].name}
+              />
+            </div>
+          )}
+          {notice?.materials[idx + 1]?.materialtype === "Text" && (
+            <div className="item">
+              <h1>{notice?.materials[idx + 1].material}</h1>
+            </div>
+          )}
+        </Fragment>
+        <Fragment>
+          {notice?.materials[idx + 2]?.materialtype === "Video" && (
+            <div className="item">
+              <ReactPlayer
+                url={notice?.materials[idx + 1].material}
+                playing={true}
+                loop={true}
+              />
+            </div>
+          )}
+          {notice?.materials[idx + 2]?.materialtype === "Image" && (
+            <div className="item">
+              <img
+                src={notice?.materials[idx + 2].material}
+                alt={notice?.materials[idx + 2].name}
+              />
+            </div>
+          )}
+          {notice?.materials[idx + 2]?.materialtype === "Text" && (
+            <div className="item">
+              <h1>{notice?.materials[idx + 2].material}</h1>
+            </div>
+          )}
+        </Fragment>
+      </div>
+    </>
+  );
 }
 export default GridSlider;
--- src/components/single_image/home.js
+++ src/components/single_image/home.js
@@ -1,18 +1,58 @@
-import React from 'react'
+import React from "react";
+import ReactPlayer from "react-player";
 
 const SingleImage = ({ notice }) => {
-    console.log(notice)
-    return (
-        <div style={{ height: "100vh" }}>
-            <div style={{ height: "100%", position: "relative" }}>
-                {notice?.materials[0]?.materialtype === 'Image' && <img src={notice.materials[0].material} alt="" style={{ height: "100%", display: "block", marginLeft: "auto", marginRight: "auto" }} />}
-                {notice?.materials[0]?.materialtype === 'Text' && <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: "100%" }}><h1>{notice.materials[0].material}</h1></div>}
-                <div style={{ position: "absolute", top: "10px", right: "10px" }}>
-                    <img src="https://i.ibb.co/dr0tvG7/The-World-Bank-Logo.png" alt="" style={{ width: "100px", borderRadius: "50%" }} />
-                </div>
-            </div>
+  console.log(notice);
+  return (
+    <div style={{ height: "100vh" }}>
+      <div style={{ height: "100%", position: "relative" }}>
+        {notice?.materials[0]?.materialtype === "Image" && (
+          <img
+            src={notice.materials[0].material}
+            alt=""
+            style={{
+              height: "100%",
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          />
+        )}
+        {notice?.materials[0]?.materialtype === "Text" && (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+            }}
+          >
+            <h1>{notice.materials[0].material}</h1>
+          </div>
+        )}
+        {notice?.materials[0]?.materialtype === "Video" && (
+          <ReactPlayer
+            url={notice.materials[0].material}
+            playing={true}
+            loop={true}
+            style={{
+              height: "100%",
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          />
+        )}
+        <div style={{ position: "absolute", top: "10px", right: "10px" }}>
+          <img
+            src="https://i.ibb.co/dr0tvG7/The-World-Bank-Logo.png"
+            alt=""
+            style={{ width: "100px", borderRadius: "50%" }}
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default SingleImage
+export default SingleImage;
